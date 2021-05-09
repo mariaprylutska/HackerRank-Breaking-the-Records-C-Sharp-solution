3889f6d5cbf53e516283562ef138765b080cd1a8
mariaprylutska <63318704+mariaprylutska@users.noreply.github.com>
2021-05-09 22:50:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        scores.ToArray();
+        int n = scores.Count();
+
+        int max = 0;
+        int min = 0;
+
+        if (n > 1)
+        {
+            int startScore = scores[0];
+            int startScore1 = scores[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (scores[i] > startScore)
+                {
+
+                    startScore = scores[i];
+                    max++;
+                }
+                else if (scores[i] < startScore1)
+                {
+
+                    startScore1 = scores[i];
+                    min++;
+                }
+            }
+        }
+
+        int[] result = new int[2];
+        result[0] = max;
+        result[1] = min;
+        return result.ToList();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
